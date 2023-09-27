24f6073882d9bd107ab80701e2de1061815381c7
mauricio <maur28363429rd@gmail.com>
2023-09-27 15:58:11 -0400

    as

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -21,7 +21,6 @@ export default boot(({ app, router }) => {
   });
 
   // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
